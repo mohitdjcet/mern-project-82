2a77d15f5e7cdfd555c09d65212dd5d17dcc7a47
Mohit Kumar <mks.djcet@gmail.com>
2026-01-29 21:26:30 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
 function App() {
-  const isLogin = false;
+  const [items, setItems] = React.useState(["React", "Vue", "Angular"]);
 
-  return(
-    <h1>
-   {
-    isLogin || dashboard
-   }
-   </h1>
-  )
+  const removeItem = (index) =>{
+    const newItems = items.filter((_, i) => i !== index);
+    setItems(newItems);
+  }
+  return (
+    <div>
+      {items.map((item, index) => (
+        <div key={index}>
+          <span>{item}</span>
+          <button onClick={()=> removeItem(index)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
 }
-
 export default App;
