211b3fc1bdd7aa034acb6add1ce3a52130a19ca0
Mohit Kumar <mks.djcet@gmail.com>
2026-01-29 21:48:45 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function App() {
-  const [items, setItems] = React.useState(["React", "Vue", "Angular"]);
+  // const inputRef = useRef(null);
 
-  const removeItem = (index) =>{
-    const newItems = items.filter((_, i) => i !== index);
-    setItems(newItems);
-  }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log("Name:", inputRef.current.value);
+  // }
+  // const [name, setName] = useState("");
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log("Name:",name);
+  // }
   return (
-    <div>
-      {items.map((item, index) => (
-        <div key={index}>
-          <span>{item}</span>
-          <button onClick={()=> removeItem(index)}>Delete</button>
-        </div>
-      ))}
-    </div>
+    // <form onSubmit={handleSubmit}>
+    //   <input
+    //     type="text"
+    //     value={name}
+    //     onChange={(e)=> setName(e.target.value)}
+    //     />
+    //     <button type="submit">Submit</button>
+    // </form>
+
+    // <form onSubmit={handleSubmit}>
+    //   <input type="text" ref={inputRef} />
+    //   <button type="submit">Submit</button>
+    // </form>
+
+    // <React.Fragment>
+    //   <h1>Hello, World!</h1>
+    //   <p>Welcome</p>
+    // </React.Fragment>
+
+   <>
+  {[1, 2, 3].map((num) => (
+    <React.Fragment key={num}>
+      <h1>Heading {num}</h1>
+      <p>This is paragraph {num}</p>
+    </React.Fragment>
+  ))}
+</>
+
   );
 }
 export default App;
