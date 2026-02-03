6ac237997de428fef8847155eea6c8e5c9ba955f
Mohit Kumar <mks.djcet@gmail.com>
2026-02-03 22:45:56 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React from "react";
-import "./App.css";
+// import { Children } from "react";
+// import useCounter from "./useCounter";
+
+import Card from "./Card";
 
 function App() {
-  const student= 1200;
-  const name= "Mohit Kumar";
+  // const { count, incerement, decrement, reset } = useCounter(null);
   return (
-    <>
-      <h1 className = "header">Hello, {5*8}!</h1>
-      <p>Total Student: {student}</p>
-    </>
+    // <>
+    //   <h1> Count : {count}</h1>
+    //   <button onClick={incerement}>Incerement</button>
+    //   <button onClick={decrement}>Decrement</button>
+    //   <button onClick={reset}>Reset</button>
+    // </>
+
+    <Card>
+      <h1> Hello from Card Component </h1>
+      <p> This is a simple card component to wrap content. </p>
+    </Card>
   );
 }
 
---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+function Card({children}) {
+    return (
+        // <div style = {{border: "1px solid black", padding: "10px", margin: "10px"}}>
+        //     {children}
+        // </div>
+        <h1>Mohit</h1>
+    )
+}
+
+export default Card;
